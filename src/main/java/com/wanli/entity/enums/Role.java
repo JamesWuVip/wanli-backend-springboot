b4b97cb1ce5e348570fb9ee4bffa5c87764902c3
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-30 20:29:32 -0700

    feat: 添加Role枚举类

---
+++ src/main/java/com/wanli/entity/enums/Role.java
@@ -0,0 +1,29 @@
+package com.wanli.entity.enums;
+
+/**
+ * 用户角色枚举
+ * 
+ * @author JamesWu
+ * @since 1.0.0
+ */
+public enum Role {
+    /**
+     * 超级管理员
+     */
+    SUPER_ADMIN,
+    
+    /**
+     * 管理员
+     */
+    ADMIN,
+    
+    /**
+     * 教师
+     */
+    TEACHER,
+    
+    /**
+     * 学生
+     */
+    STUDENT
+}

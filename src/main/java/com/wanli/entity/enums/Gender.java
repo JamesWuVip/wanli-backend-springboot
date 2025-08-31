5347ce2d8e1670c12a5f9c08f6a942684cd8b21a
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-30 20:29:24 -0700

    feat: 添加Gender枚举类

---
+++ src/main/java/com/wanli/entity/enums/Gender.java
@@ -0,0 +1,24 @@
+package com.wanli.entity.enums;
+
+/**
+ * 性别枚举
+ * 
+ * @author JamesWu
+ * @since 1.0.0
+ */
+public enum Gender {
+    /**
+     * 男性
+     */
+    MALE,
+    
+    /**
+     * 女性
+     */
+    FEMALE,
+    
+    /**
+     * 其他
+     */
+    OTHER
+}

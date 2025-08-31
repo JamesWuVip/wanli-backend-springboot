90af5999e98f153ac66eecb297c6d933f0d266e2
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-30 20:29:40 -0700

    feat: 添加UserStatus枚举类

---
+++ src/main/java/com/wanli/entity/enums/UserStatus.java
@@ -0,0 +1,29 @@
+package com.wanli.entity.enums;
+
+/**
+ * 用户状态枚举
+ * 
+ * @author JamesWu
+ * @since 1.0.0
+ */
+public enum UserStatus {
+    /**
+     * 激活状态
+     */
+    ACTIVE,
+    
+    /**
+     * 未激活状态
+     */
+    INACTIVE,
+    
+    /**
+     * 暂停状态
+     */
+    SUSPENDED,
+    
+    /**
+     * 锁定状态
+     */
+    LOCKED
+}
